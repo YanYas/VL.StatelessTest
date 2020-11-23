b86873bec52410e051a75f28e57e74e3a326fa47
yanyas <hayden.anyasi@network.rca.ac.uk>
2020-11-23 09:24:10 +0100

    First Commit

---
+++ src/StateEnums/StateEnumsMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StateEnums
+{
+    public class MyStateEnums
+    {
+        public enum StateEn{ New, Filled, Shipping, Complete, Cancelled }
+        public enum TriggerEn { ItemsConfirmed, PurchaseConfirmed, DeliveryConfirmed, DeliveryCancelled, BeginNewPurchase }
+
+        public static string StateString(StateEn e)
+        {
+            return e.ToString();
+        }
+
+        public static string TriggerString(TriggerEn e)
+        {
+            return e.ToString();
+        }
+    }
+}
